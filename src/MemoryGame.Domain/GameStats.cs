9cc035d73a2f71100b34a33f97ecde0be748990d
David Armstrong <proxie89@gmail.com>
2025-08-12 10:03:39 +0200

    Card & GameStats

---
+++ src/MemoryGame.Domain/GameStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.Domain;
+
+public class GameStats
+{
+    public string PlayerName { get; init; }
+    public int Moves { get; init; }
+    public TimeSpan GameTime { get; init; }
+    public DateTime CompletedAt { get; init; }
+
+}
