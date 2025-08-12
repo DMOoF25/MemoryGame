253a235fdd2ce83651a7b74bf8347515a7d1247d
David Armstrong <proxie89@gmail.com>
2025-08-12 10:35:48 +0200

    gameEngine

---
+++ src/MemoryGame.Application/GameEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemoryGame.Domain;
+
+namespace MemoryGame.Application;
+
+public sealed class GameEngine
+{
+    private List<Card> _deck = new();
+    private DateTime startTimer;
+    private DateTime endTimer;
+
+    public ReadOnlyCollection<Card> Deck => _deck.AsReadOnly();
+    public string userName  { get; set; } = string.Empty;
+
+    public int Moves { get; set; }
+
+    public TimeSpan ElapsedTime { get; set; }
+
+    public bool IsRunning { get; set; }
+
+}
